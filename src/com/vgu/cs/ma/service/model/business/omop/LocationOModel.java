9fdd0f7617b078a83d02985e2d03207d37f2b3fa
namnh16 <hnam9430@gmail.com>
2021-04-25 15:51:27 +0700

    constructOmop for Location (location)

--- src/com/vgu/cs/ma/service/model/business/omop/LocationOModel.java
+++ src/com/vgu/cs/ma/service/model/business/omop/LocationOModel.java
@@ -8,10 +8,10 @@ package com.vgu.cs.ma.service.model.business.omop;
  */
 
 import com.vgu.cs.common.util.ConvertUtils;
-import com.vgu.cs.common.util.StringUtils;
 import com.vgu.cs.engine.dal.LocationDal;
 import com.vgu.cs.engine.entity.LocationEntity;
 import org.hl7.fhir.dstu3.model.Address;
+import org.hl7.fhir.dstu3.model.Location;
 
 public class LocationOModel {
     
@@ -19,6 +19,19 @@ public class LocationOModel {
     
     private LocationOModel() {
     
+    }
+    
+    /**
+     * Unretained fields include:
+     * - location_source_value
+     */
+    public LocationEntity constructOmop(Location fLocation) {
+        LocationEntity oLocation = new LocationEntity();
+        
+        _setId(fLocation, oLocation);
+        _setAddress(fLocation, oLocation);
+        
+        return oLocation;
     }
     
     public Address getAddress(int locationId) {
@@ -38,13 +51,8 @@ public class LocationOModel {
         address.setState(location.state);
         address.setPostalCode(location.zip);
         address.setCountry(location.county);
-        if (!StringUtils.isNullOrEmpty(location.address_1) && !StringUtils.isNullOrEmpty(location.address_2)) {
-            address.addLine(location.address_1 + " " + location.address_2);
-        } else if (!StringUtils.isNullOrEmpty(location.address_1)) {
-            address.addLine(location.address_1);
-        } else if (!StringUtils.isNullOrEmpty(location.address_2)) {
-            address.addLine(location.address_2);
-        }
+        address.addLine(location.address_1);
+        address.addLine(location.address_2);
         
         return address;
     }
@@ -54,5 +62,33 @@ public class LocationOModel {
             return 0;
         }
         return ConvertUtils.toInteger(address.getId());
+    }
+    
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Private
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    /**
+     * LOCATION.location_id = Location.id
+     */
+    private void _setId(Location fLocation, LocationEntity oLocation) {
+        oLocation.location_id = ConvertUtils.toInteger(fLocation.getId());
+    }
+    
+    /**
+     * Maps Location.address to LOCATION's city, state, zip, county, address_1, address_2
+     */
+    private void _setAddress(Location fLocation, LocationEntity oLocation) {
+        Address address = fLocation.getAddress();
+        if (address == null) {
+            return;
+        }
+        
+        oLocation.city = address.getCity();
+        oLocation.state = address.getState();
+        oLocation.zip = address.getPostalCode();
+        oLocation.county = address.getCountry();
+        oLocation.address_1 = address.getLine().get(0).toString();
+        oLocation.address_2 = address.getLine().get(1).toString();
     }
 }
--- test/Logic.java
+++ test/Logic.java
@@ -6,25 +6,24 @@
  */
 
 import com.vgu.cs.engine.entity.CareSiteEntity;
+import com.vgu.cs.engine.entity.LocationEntity;
 import com.vgu.cs.engine.entity.VisitOccurrenceEntity;
 import com.vgu.cs.ma.service.model.business.fhir.EncounterFModel;
 import com.vgu.cs.ma.service.model.business.fhir.LocationFModel;
 import com.vgu.cs.ma.service.model.business.omop.CareSiteOModel;
+import com.vgu.cs.ma.service.model.business.omop.LocationOModel;
 import com.vgu.cs.ma.service.model.business.omop.VisitOccurrenceOModel;
 import com.vgu.cs.ma.service.model.data.CareSiteDModel;
 import com.vgu.cs.ma.service.model.data.ConceptDModel;
+import com.vgu.cs.ma.service.model.data.LocationDModel;
 import com.vgu.cs.ma.service.model.data.VisitOccurrenceDModel;
 
 public class Logic {
     
     public static void main(String[] args) {
-        CareSiteEntity omop = CareSiteDModel.INSTANCE.getCareSite(1);
+        LocationEntity omop = LocationDModel.INSTANCE.getLocation(1);
         System.out.println(omop);
-        System.out.println(CareSiteOModel.INSTANCE.constructOmop(LocationFModel.INSTANCE.constructFhir(omop)));
-    
-//        System.out.println(ConceptDModel.INSTANCE.getConcept(8562));
-//        System.out.println(ConceptDModel.INSTANCE.getConcept(8536));
-//        System.out.println(ConceptDModel.INSTANCE.getConcept(44818517));
+        System.out.println(LocationOModel.INSTANCE.constructOmop(LocationFModel.INSTANCE.constructFhir(omop)));
         
         System.exit(0);
     }
